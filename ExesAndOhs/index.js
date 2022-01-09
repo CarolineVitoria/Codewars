d760f2a1b59b92282d1b34bf302407ebe9f4fa09
Caroline Vitóra <carolmelo007@hotmail.com>
2022-01-09 09:18:22 -0300

    kyu 7

--- ExesAndOhs/index.js
+++ ExesAndOhs/index.js
@@ -1,22 +1,6 @@
-// function XO(str) {
-//     let x = 0;
-//     let o = 0;
-//     for(const letter of str){
-//         if(letter === 'x' || letter === 'X'){
-//             x+=1;
-//         }
-//         if(letter === 'o' || letter === 'O'){
-//             o+=1;
-//         }
-//     }
-//     if(x%o===0){
-//         return console.log(true);
-//     }
-//     else{
-//         return console.log(false);
-//     }
-// }
-// XO('xxooox');
+//Check to see if a string has the same amount of 'x's and 'o's. 
+//The method must return a boolean and be case insensitive. 
+//The string can contain any char.
 function XO(str) {
     let x = 0;
     let o = 0;
@@ -31,5 +15,4 @@ function XO(str) {
     }
     x===o ? console.log(true): console.log(false);
   }
-  XO("o");
-  console.log(0/2);
+  XO("xoxo");
